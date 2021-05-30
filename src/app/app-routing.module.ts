b3be371f67f9de7d9ac480096359e5b403228539
Sergio Gomez <laragsergio1803@gmail.com>
2021-05-30 16:23:20 -0500

    se craron vistas de login y registro, se ajusto paleta de colores y prototipo del home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'login', 
+    component: LoginComponent,
+    //canActivate: [LoginGuard]
+  },
+  {
+    path : 'registro', 
+    component: RegistroComponent,
+    //canActivate: [LoginGuard]
+  },
+  {
+    path : 'home', 
+    component: HomeComponent,
+    //canActivate: [LoginGuard]
+  },
+  {path : '', pathMatch:'full', redirectTo: 'home'},
+  {path : '**', pathMatch:'full', redirectTo: 'home'}
 
+];
+
+const config: ExtraOptions = {
+  useHash: true,
+};
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,config)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
